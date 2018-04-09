41506a5c6adc36fc9ad5adb06df6d7762bc06ca4
Zuzana O <obajtek.zuzanna@gmail.com>
2018-04-09 21:57:25 +0200

    Matrix - rozwiązanie

--- lab5/matrix/Matrix.h
+++ lab5/matrix/Matrix.h
@@ -5,10 +5,44 @@
 #ifndef JIMP_EXERCISES_MATRIX_H
 #define JIMP_EXERCISES_MATRIX_H
 
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <complex>
+#include <vector>
 
-class Matrix {
+using std::string;
 
-};
+namespace algebra {
+    class Matrix {
+        std::complex<double> **mac;
+        int n, m;
+
+    public:
+        Matrix(int n, int m); //parametrowy
+        Matrix();             //bezparam
+        Matrix(Matrix &matrix);
+        Matrix(string matlab); //matlaba
+        Matrix(std::initializer_list<std::vector<std::complex<double>>> lista);
+
+        //ustawianie, pobieranie elementów
+        std::pair<size_t, size_t>  Size();
+        string Print();
+        std::complex<double> ** macierz();
+        int wysokosc();
+        int szerokosc();
+
+
+        //działania
+        Matrix Add(Matrix macierz); //wymiary!
+        Matrix Sub(Matrix macierz); //wymiary!
+        Matrix Mul(Matrix macierz); //wymiary!
+        Matrix Mul(std::complex<double> skalar);
+        Matrix Pow(int potega);
+
+        ~Matrix();
+    };
+}
 
 
 #endif //JIMP_EXERCISES_MATRIX_H
